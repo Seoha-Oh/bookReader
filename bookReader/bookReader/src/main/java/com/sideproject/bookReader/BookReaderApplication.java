99d118655d81201696a6c1704df047f73ebb60cf
seoha <seoha8952@gmail.com>
2024-11-13 16:57:23 +0900

    first init

---
+++ bookReader/bookReader/src/main/java/com/sideproject/bookReader/BookReaderApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.bookReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReaderApplication.class, args);
+	}
+
+}
